e551f0a8a9eb0b58030be9731016bc29ce7e15e0
alessandrodd <didiego.alessandro@gmail.com>
2026-03-22 16:37:47 +0100

    update SECRET_PASSWORD constant.

--- source/src/game-config.ts
+++ source/src/game-config.ts
@@ -1,3 +1,3 @@
 export const GAME_TITLE = 'Fly Laura, Fly!';
 export const PASSWORD_TARGET_SCORE = 26;
-export const SECRET_PASSWORD = 'ECB-CINEMA-23';
+export const SECRET_PASSWORD = '26PointsToPopcorn';
